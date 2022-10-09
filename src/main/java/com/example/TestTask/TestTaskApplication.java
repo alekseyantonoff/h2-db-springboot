ba58e28f2ace13d7de10e63d47b403bec5c9bbf7
Aleksey Antonov <46189617+alekseyantonoff@users.noreply.github.com>
2022-10-09 14:07:07 +0700

    Add files via upload

---
+++ src/main/java/com/example/TestTask/TestTaskApplication.java
@@ -0,0 +1,12 @@
+package com.example.TestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class TestTaskApplication {
+	public static void main(String[] args) throws Throwable{
+		SpringApplication.run(TestTaskApplication.class, args);
+	}
+}
