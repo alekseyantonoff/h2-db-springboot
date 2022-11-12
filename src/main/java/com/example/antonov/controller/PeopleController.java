f3b42e203be4dd0717b4a61d37823a0525adc742
aantonov <aleksey.antonoff@gmail.com>
2022-11-12 21:38:09 +0700

    Переписал маппинги в PeopleController

--- src/main/java/com/example/antonov/controller/PeopleController.java
+++ src/main/java/com/example/antonov/controller/PeopleController.java
@@ -7,25 +7,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static com.example.antonov.controller.PeopleController.ROOT_URL;
-
 @RestController
-@RequestMapping(ROOT_URL)
+@RequestMapping("/persons")
 @RequiredArgsConstructor
 public class PeopleController {
 
     private final PersonService personService;
 
-
-    public final static String ROOT_URL = "/persons";
-    public final static String NEW_PATH = "/new";
-    public final static String EDIT_PATH = "/{id}/edit";
-    public final static String DELETE_PATH = "/{id}/delete";
-    public final static String ID_PATH = "/{id}";
-    public final static String FIND_PATH = "/find";
-
-
-    @GetMapping(NEW_PATH)
+    @GetMapping("/new")
     public PersonDto createPerson(@RequestParam("name") String name) {
         return personService.createPerson(name);
     }
@@ -35,22 +24,22 @@ public class PeopleController {
         return personService.showAllPersons();
     }
 
-    @GetMapping(ID_PATH)
+    @GetMapping("/{id}")
     public PersonDto showPerson(@PathVariable Long id){
         return personService.showPerson(id);
     }
 
-    @RequestMapping(EDIT_PATH)
+    @RequestMapping("/{id}/edit")
     public PersonDto editPerson(@PathVariable Long id, @RequestParam("name") String name){
         return personService.editPerson(id, name);
     }
 
-    @RequestMapping(DELETE_PATH)
+    @RequestMapping("/{id}/delete")
     public void deletePerson(@PathVariable Long id){
         personService.deletePerson(id);
     }
 
-    @GetMapping(FIND_PATH)
+    @GetMapping("/find")
     public List<PersonDto> findByName(@RequestParam("name") String name) {
         return personService.findByName(name);
     }
